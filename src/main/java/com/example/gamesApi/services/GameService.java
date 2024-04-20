20631d74bcaa93d981ff6d162d42f8308d29132f
raffasantos <raafael.cs@gmail.com>
2024-04-19 23:46:03 -0300

    Implementing business rules in the Service class and creating the GetAll and Create method.

--- src/main/java/com/example/gamesApi/services/GameService.java
+++ src/main/java/com/example/gamesApi/services/GameService.java
@@ -1,99 +1,108 @@
 package com.example.gamesApi.services;
 
 import com.example.gamesApi.dto.GameRecordDto;
-import com.example.gamesApi.entities.Game;
+import com.example.gamesApi.models.GameModel;
 import com.example.gamesApi.repositories.GameRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.List;
 
 @Service
 public class GameService {
 
-
     @Autowired
     GameRepository gameRepository;
-    final DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm");
 
+    final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
     final Calendar calendar = Calendar.getInstance();
 
-    @Transactional
-    public Object createGame(GameRecordDto gameRecordDto){
-        if(gameRecordDto == null){
-            return "Enter a valid request body";
+
+    public List<GameModel> getAll(){
+        return gameRepository.findAll();
+    }
+
+    public Object create(GameRecordDto gameRecordDto) {
+
+        var genres = gameRecordDto.getGenres();
+        var platforms = gameRecordDto.getPlatforms();
+
+        //verifying the genres attribute
+        for (int i = 0; i <= genres.length - 1; i++) {
+            if (genres[i].equalsIgnoreCase("Action")
+                    || genres[i].equalsIgnoreCase("Action-Adventure")
+                    || genres[i].equalsIgnoreCase("Adventure")
+                    || genres[i].equalsIgnoreCase("Card Game")
+                    || genres[i].equalsIgnoreCase("Casual")
+                    || genres[i].equalsIgnoreCase("City Builder")
+                    || genres[i].equalsIgnoreCase("Comedy")
+                    || genres[i].equalsIgnoreCase("Dungeon Crawler")
+                    || genres[i].equalsIgnoreCase("Exploration")
+                    || genres[i].equalsIgnoreCase("Fantasy")
+                    || genres[i].equalsIgnoreCase("Fighting")
+                    || genres[i].equalsIgnoreCase("First Person")
+                    || genres[i].equalsIgnoreCase("Horror")
+                    || genres[i].equalsIgnoreCase("Indie")
+                    || genres[i].equalsIgnoreCase("Moba")
+                    || genres[i].equalsIgnoreCase("Music")
+                    || genres[i].equalsIgnoreCase("Narration")
+                    || genres[i].equalsIgnoreCase("Open World")
+                    || genres[i].equalsIgnoreCase("Party")
+                    || genres[i].equalsIgnoreCase("Platformer")
+                    || genres[i].equalsIgnoreCase("Puzzle")
+                    || genres[i].equalsIgnoreCase("Racing")
+                    || genres[i].equalsIgnoreCase("Retro")
+                    || genres[i].equalsIgnoreCase("Rogue-Lite")
+                    || genres[i].equalsIgnoreCase("RTS")
+                    || genres[i].equalsIgnoreCase("Shooter")
+                    || genres[i].equalsIgnoreCase("Space")
+                    || genres[i].equalsIgnoreCase("Stealth")
+                    || genres[i].equalsIgnoreCase("Survival")
+                    || genres[i].equalsIgnoreCase("Tower Defense")
+                    || genres[i].equalsIgnoreCase("Trivia")
+                    || genres[i].equalsIgnoreCase("Turn-Based")
+                    || genres[i].equalsIgnoreCase("Turn-Based Strategy")
+                    || genres[i].equalsIgnoreCase("RPG")
+                    || genres[i].equalsIgnoreCase("Simulation")
+                    || genres[i].equalsIgnoreCase("Strategy")
+                    || genres[i].equalsIgnoreCase("Sports")
+                    || genres[i].equalsIgnoreCase("Battle Royale")) {
+
+            } else {
+                return "Enter valid genres: Action, Action-Adventure, Adventure, Card Game, Casual, City Builder, Comedy, Dungeon Crawler, Exploration, Fantasy, Fighting, First Person, " +
+                        "Horror, Indie, Moba, Music, Narration, Open World, Party, Platformer, Puzzle, Racing, Retro, Rogue-Lite, RTS, Shooter, Space, Stealth, Survival, Tower Defense, " +
+                        "Trivia, Turn-Based, Turn-Based Strategy, RPG, Simulation, Strategy, Sports, Battle Royale.";
+            }
         }
 
-        var gameModel = new Game();
-        BeanUtils.copyProperties(gameRecordDto, gameModel);
 
-        if(gameModel.getTitle().isEmpty()){
-            return "Enter a valid Title.";
+        //verifying the genres attribute
+        for(int i=0; i <= platforms.length-1; i++){
+            if(platforms[i].equalsIgnoreCase("PS5")
+                    || platforms[i].equalsIgnoreCase("PS4")
+                    || platforms[i].equalsIgnoreCase("Xbox 360")
+                    || platforms[i].equalsIgnoreCase("Xbox One")
+                    || platforms[i].equalsIgnoreCase("PC")
+                    || platforms[i].equalsIgnoreCase("Nintendo Switch")
+                    || platforms[i].equalsIgnoreCase("Android")
+                    || platforms[i].equalsIgnoreCase("iOS")){
+
+            } else {
+                return "Enter valid platforms: PS5, PS4, Xbox 360, Xbox One, PC, Nintendo Switch, Android, iOS";
+            }
         }
-        if(gameModel.getUrlImage().isEmpty()){
-            return "Enter a valid URL for the game image.";
-        }
-        if(gameModel.getUrlBanner().isEmpty()){
-            return "Enter a valid URL for the game banner.";
-        }
-        if(gameModel.getReleaseDate().isEmpty()){
-            return "Enter a synopsis.";
-        }
-        if(gameModel.getSynopsis().isEmpty()){
-            return "Enter a synopsis.";
-        }
-        if(gameModel.getGenres() == null){
-            return "Enter valid genres: Action, Action-Adventure, Adventure, Card Game, Casual, City Builder, Comedy, Dungeon Crawler, Exploration, Fantasy, Fighting, First Person, Horror, Indie, Moba, Music, Narration, Open World, Party, Platformer, Puzzle, Racing, Retro, Rogue-Lite, RTS, Shooter, Space, Stealth, Survival, Tower Defense, Trivia, Turn-Based, Turn-Based, RPG, Simulation, Strategy, Sports, Battle Royale.";
-        }
-        if(gameModel.getPlatforms() == null){
-            return "Enter a valid platform: PS5, PS4, Xbox 360, Xbox One, PC, Nintendo Switch, Android, iOS.";
-        }
+
+
+        GameModel gameModel = new GameModel(gameRecordDto);
 
         gameModel.setAddedTime(dateFormat.format(calendar.getTime()));
 
-        return gameRepository.save(gameModel);
-
+        return gameModel;
     }
-
-    public Object updateGame(UUID id, GameRecordDto gameRecordDto){
-        Optional<Game> gameO = gameRepository.findById(id);
-        if(gameO.isEmpty()){
-            return "Game not found";
-        }
-
-        var gameModel = gameO.get();
-        BeanUtils.copyProperties(gameRecordDto, gameModel);
-
-        if(gameModel.getTitle().isEmpty()){
-            return "Enter a valid Title.";
-        }
-        if(gameModel.getUrlImage().isEmpty()){
-            return "Enter a valid URL for the game image.";
-        }
-        if(gameModel.getUrlBanner().isEmpty()){
-            return "Enter a valid URL for the game banner.";
-        }
-        if(gameModel.getReleaseDate().isEmpty()){
-            return "Enter a synopsis.";
-        }
-        if(gameModel.getSynopsis().isEmpty()){
-            return "Enter a synopsis.";
-        }
-        if(gameModel.getGenres() == null){
-            return "Enter valid genres: Action, Adventure, RPG, Simulation, Strategy, Sport, Battle Royale.";
-        }
-        if(gameModel.getPlatforms() == null){
-            return "Enter a valid platform: PS5, PS4, Xbox 360, Xbox One, PC, Nintendo Switch, Android, iOS.";
-        }
-
-        return gameRepository.save(gameModel);
-    }
-
 
 }
